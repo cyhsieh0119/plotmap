50c3fca460cc3847b8fe4d0f83aaf1ee83cf6266
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-19 12:52:55 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import cv2
+import streamlit as st
+import numpy as np
+import scipy.stats as sst
+impoet plotly.express as px
+
+st.set_page_config(page_title="Plot Map Exercise !" ,page_icon="random" ,layout="wide")
+
+st.markdown("# Plot XY map for some parameters！🎈")
+st.sidebar.markdown("# Main page 🎈 of Side Bar")
+
+
+def main():
+	st.title("File Upload Tutorial")
+	menu = ["Dataset","DocumentFiles","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+	tab1, tab2, tab3 = st.tabs(["SPI", "DB", "AOI"])
+
+	if choice == "Dataset":
+		st.subheader("Dataset")
+		data_file = st.file_uploader(r"Upload *.csv or *.csv.zip", type=["csv","zip"])
+		if data_file is not None:
+			file_details = {"filename":data_file.name, "filetype":data_file.type,
+				 	            "filesize":image_file.size}
+			st.sidebar.write(file_details)
+			# To View Uploaded Image
+			with tab1:
+				st.header(data_file.name)				
+
+	elif choice == "DocumentFiles":
+		st.subheader("DocumentFiles")
+
+
+
+
+if __name__ == '__main__':
+	#initD()
+	main()
