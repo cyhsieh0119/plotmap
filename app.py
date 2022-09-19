2e391e80e026b918b5a11eb3fb2961b4b305a3c9
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-19 19:55:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def main():
 			file_details = {"filename":data_file.name, "filetype":data_file.type,
 				 	"filesize":data_file.size}
 			df = pd.read_csv(data_file, skiprows=10, engine='c',
-					 encoding='latin1',encoding_errors='ignore')
+					 sep=",", compression="zip",
+					 encoding='ISO-8859-1',encoding_errors='ignore')
 			spi_items = [ 'Layout No.', 'Pin No.' , 'Pad No.' , 'Area[um2]' , 'Area[%]' , 'X shift' , 'Y shift', 
 				     'No solder' , 'Center X', 'Center Y',             ]
 			df1 = df.loc[:,spi_items]
