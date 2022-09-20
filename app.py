1e9b71bcd828012cb8d6bb3e6f5d22be51cb48ea
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-20 12:05:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,15 +35,22 @@ def main():
 				panel_id = data_file.name
 				fitem = 'Pin No.'
 				xysize=[450,450]
-				figA = plotSPI(df1, panel_id , spi_check[1], fitem, 3, [50, 150], xysize)
-				figx = plotSPI(df1, panel_id , spi_check[2], fitem, 3, [-25, 25], xysize)
-				figy = plotSPI(df1, panel_id , spi_check[3], fitem, 3, [-25, 25], xysize)
+					
 				col1, col2, col3 = st.columns(3)
 				with col1:
+					values = st.slider('Select a range of Areas',50.0, 150.0, (70.0, 130.0))
+					minA, maxA = values[0], [1]
+					figA = plotSPI(df1, panel_id , spi_check[1], fitem, 3, [minA, maxA], xysize)
 					st.plotly_chart(figA, use_container_width=True)
 				with col2:
+					values = st.slider('Select a range of Areas',-40.0, 40.0, (-25.0, 25.0))
+					minX, maxX = values[0], [1]
+					figx = plotSPI(df1, panel_id , spi_check[2], fitem, 3, [minX, maxX], xysize)
 					st.plotly_chart(figx, use_container_width=True)
 				with col3:
+					values = st.slider('Select a range of Areas',-40.0, 40.0, (-25.0, 25.0))
+					minY, maxY = values[0], [1]
+					figy = plotSPI(df1, panel_id , spi_check[3], fitem, 3, [minY, maxY], xysize)
 					st.plotly_chart(figy, use_container_width=True)
 				
 				#st.plotly_chart(figA, use_container_width=False)
