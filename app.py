1abdd939c5a4794e951133e8523aca2485f60700
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-20 08:08:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-from .utils import read_SPI_file, plotSPI
+from utils import read_SPI_file, plotSPI
 
 st.set_page_config(page_title="Plot Map Exercise !" ,page_icon="random" ,layout="wide")
 
