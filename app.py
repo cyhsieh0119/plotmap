8080eeb146930cdfcae8e3003452826a6f57a676
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-20 08:21:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,16 @@ def main():
 	st.title("File Upload Tutorial")
 	menu = ["Dataset","DocumentFiles","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
-	tab1, tab2, tab3 = st.tabs(["SPI", "DB", "AOI"])
+	#tab1, tab2, tab3 = st.tabs(["SPI", "DB", "AOI"])
 
 	if choice == "Dataset":
 		st.subheader("Dataset")
+		tab1, tab2 = st.tabs(["Graph", "Detail Data"])
 		data_file = st.file_uploader(r"Upload *.csv or *.csv.zip", type=["csv","zip"])
 		if data_file is not None:
 			file_details = {"filename":data_file.name, "filetype":data_file.type,
 				 	"filesize":data_file.size}
+			st.sidebar.write(file_details)
 			df1 = read_SPI_file(data_file)
 			#df = pd.read_csv(data_file, skiprows=10, engine='c',
 			#		 sep=",", compression="zip", low_memory=False, 
@@ -34,12 +36,17 @@ def main():
 			#df1['Layout No.'] = df1['Layout No.'].astype('int32')
 			#df1.sort_values(['Layout No.', 'Pin No.'], inplace = True )
 
-
-			st.write(df1)
-			st.sidebar.write(file_details)
-			# To View Uploaded Image
 			with tab1:
-				st.header(data_file.name)				
+				fitem = 'Pin No.'
+    				xysize=[600,600]
+    				figA = plotSPI(df1, panel_id , spi_check[1], fitem, 3, [50, 150], xysize)
+    				figx = plotSPI(df1, panel_id , spi_check[2], fitem, 3, [-25, 25], xysize)
+    				figy = plotSPI(df1, panel_id , spi_check[3], fitem, 3, [-25, 25], xysize)
+			
+			
+			# To View Uploaded Image
+			with tab2:
+				st.write(df1)				
 
 	elif choice == "DocumentFiles":
 		st.subheader("DocumentFiles")
