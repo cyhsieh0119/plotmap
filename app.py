b236d9b5f613dee23285c86fee71b9ae70afa6e7
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-22 12:00:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,31 +8,41 @@ from utils import read_SPI_file, plotSPI, read_db_zip,  plotDB
 
 st.set_page_config(page_title = "Plot Map!" ,page_icon="random" ,layout="wide", initial_sidebar_state="collapsed")
 st.sidebar.markdown("# Please Select the Items:")
-#st.title("# Plot XY Map for Some Parameter！")
 
 def main():
-	#st.title("File Upload Tutorial")
-	st.title("# Plot XY Map for Some Parameter！")
 	menu = ["SPI","DB","AOI"]
-	choice = st.sidebar.selectbox("Menu(Only SPI is OK!)",menu)
-	db_zip_file, data_file = ' ' , ' '
+	choice = st.sidebar.selectbox("Menu(Only SPI/DB is OK!)",menu)
+	st.title(f"# Plot XY Map for {choice} information！")
 
 	if choice == "SPI":
-		if data_file is not None:
-			del data_file
-		st.subheader("Please use *.csv.zip as the input dataset file !")
-		tcol1, tcol2, tcol3 = st.columns([3,1,5])
-		with tcol1:
-			data_file = st.file_uploader(r"Upload *.csv.zip", type=["zip"])
+		df1 = None
+		with st.form("spi-form", clear_on_submit=True):
+			tcol1, tcol2, tcol3 = st.columns([3,1,5])
+			with tcol1:
+				data_file = st.file_uploader(r"Upload *.csv.zip", type=["zip"])
+				submitted = st.form_submit_button("UPLOAD!")
+
+				if submitted and db_zip_file is not None:
+					st.write("UPLOADED!")
+					with tcol3:
+						file_details = {"filename":db_zip_file.name, "filetype":db_zip_file.type, "filesize":db_zip_file.size}
+						st.write(file_details)
+					df1 = read_SPI_file(data_file)
+		
+		######
+		#tcol1, tcol2, tcol3 = st.columns([3,1,5])
+		#with tcol1:
+		#	data_file = st.file_uploader(r"Upload *.csv.zip", type=["zip"])
 		#
 		tab1, tab2 = st.tabs(["XY Map", "Histogram"])
-		if data_file is not None:
-			with tcol3:
-				file_details = {"filename":data_file.name, "filetype":data_file.type, "filesize":data_file.size}
-				st.write(file_details)
-				#st.sidebar.write(file_details)
+		#if data_file is not None:
+		if submitted and df1 is not None:
+			#with tcol3:
+			#	file_details = {"filename":data_file.name, "filetype":data_file.type, "filesize":data_file.size}
+			#	st.write(file_details)
 			#
-			df1 = read_SPI_file(data_file)
+			#df1 = read_SPI_file(data_file)
+			
 			spi_check=['Area[um2]', 'Area[%]',  'X shift', 'Y shift' ]
 
 			with tab1:
@@ -42,29 +52,21 @@ def main():
 					
 				col1, col2, col3, col4, col5  = st.columns([9,1,9,1,9])
 				with col1:
-					#Acol1, Acol2 = st.columns([3,1])
-					#with Acol1:
 					valueA = st.slider('Select a range of Areas',50, 150, (50, 150))
 					minA, maxA = valueA[0], valueA[1]
 					figA = plotSPI(df1, panel_id , spi_check[1], fitem, 3, [minA, maxA], xysize)
 					st.plotly_chart(figA, use_container_width=True)
 				with col3:
-					#Xcol1, Xcol2 = st.columns([3,1])
-					#with Xcol1:
 					valueX = st.slider('Select a range of X shift',-40, 40, (-25, 25))
 					minX, maxX = valueX[0], valueX[1]
 					figx = plotSPI(df1, panel_id , spi_check[2], fitem, 3, [minX, maxX], xysize)
 					st.plotly_chart(figx, use_container_width=True)
 				with col5:
-					#Ycol1, Ycol2 = st.columns([3,1])
-					#with Ycol1:
 					valueY = st.slider('Select a range of Y shift',-40, 40, (-25, 25))
 					minY, maxY = valueY[0], valueY[1]
 					figy = plotSPI(df1, panel_id , spi_check[3], fitem, 3, [minY, maxY], xysize)
 					st.plotly_chart(figy, use_container_width=True)
 				
-
-			# To View Uploaded Image
 			with tab2:
 				hcol1, hcol2, hcol3 = st.columns(3)
 				with hcol1:
@@ -78,9 +80,8 @@ def main():
 					st.write(df1)				
 
 	elif choice == "DB":
-		st.subheader("DB is under constructed! Coming Soon...")
 		df1 = None
-		with st.form("my-form", clear_on_submit=True):
+		with st.form("db-form", clear_on_submit=True):
 			db_col1, db_col2, db_col3 = st.columns([3,1,5])
 			with db_col1:
 				db_zip_file = st.file_uploader(r"Upload *.csv.zip", type=["zip"])
