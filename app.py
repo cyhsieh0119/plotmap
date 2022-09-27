d98b30b7d4c7643aedc9bd35b9114f66a5d389cf
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-27 19:30:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,28 +15,25 @@ def main():
 	
 	if choice == "SPI/DB":	
 		mcol1, mcol2, mcol3 = st.columns([5,0.1,5])
-		with mcol1:
+		with st.form("spi-form", clear_on_submit=True):
 			#
-			df_spi = None
-			with st.form("spi-form", clear_on_submit=True):
+			with mcol1:
+				df_spi = None
 				data_file = st.file_uploader(r"SPI Upload *.csv.zip", type=["zip"])
-				submitted = st.form_submit_button("SPI UPLOAD!")
-				if submitted and data_file is not None:
-					st.write("UPLOADED!")
-					spi_file_details = rf"filename:{data_file.name},\n filetype:{data_file.type},\n filesize:{data_file.size}"
-					st.success(spi_file_details, icon="✅")
-					#st.info(spi_file_details, icon="i")
-					df_spi = read_SPI_file(data_file)
-		with mcol3:
-			df_db = None
-			with st.form("db-form", clear_on_submit=True):
+			with mcol3:
+				df_db = None
 				db_zip_file = st.file_uploader(r"DB Upload *.csv.zip", type=["zip"])
-				submitted_db = st.form_submit_button("DB UPLOAD!")
-				if submitted_db and db_zip_file is not None:
-					st.write("UPLOADED!")
+			#
+			submitted = st.form_submit_button("UPLOAD!")
+			if submitted and db_zip_file is not None:
+				st.write("UPLOADED!")
+				if data_file is not None:	
+					spi_file_details = rf"filename:{data_file.name},\n filetype:{data_file.type},\n filesize:{data_file.size}"
+					#st.success(spi_file_details, icon="✅")
+					df_spi = read_SPI_file(data_file)
+				if db_zip_file is not Non
 					db_file_details = rf"filename:{db_zip_file.name},\n filetype:{db_zip_file.type},\n filesize:{db_zip_file.size}"
-					st.success(db_file_details, icon="✅")
-					#st.info(db_file_details, icon="i")
+					#st.success(db_file_details, icon="✅")
 					df_db = read_db_zip(db_zip_file)
 #
 		st.subheader('XY MAP')
@@ -61,7 +58,7 @@ def main():
 				st.plotly_chart(figA, use_container_width=True)				
 	#
 		with gcol3:
-			if submitted_db and df_db is not None:
+			if submitted and df_db is not None:
 				if df_db.shape[0] == 40994:
 					xysize=[500, 500]
 					panel_id=df_db['Panel_ID'].unique()[0]
